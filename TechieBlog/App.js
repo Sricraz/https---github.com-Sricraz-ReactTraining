2a3468826a295ba29fc265ab9883313755493874
Sriyamini1608 <156992205+Sriyamini1608@users.noreply.github.com>
2024-01-25 19:20:24 +0530

    first commit

---
+++ TechieBlog/App.js
@@ -0,0 +1,15 @@
+import react from "react"
+/*import {BrowserRouter as Router,
+    Switch,
+    Route,
+
+} from "react-router-dom";*/
+const App =() =>{
+    return <>
+  App
+    </>
+}
+
+
+
+export default App
