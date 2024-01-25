2db9936e7d5b0a99db15d1df054b0abd583614aa
Sriyamini1608 <156992205+Sriyamini1608@users.noreply.github.com>
2024-01-25 12:44:09 +0530

    first commit

---
+++ Day1/Script.js
@@ -0,0 +1,11 @@
+const clickMebtn = document.getElementById("button-id");
+clickMebtn.addEventListener("click", () => {
+  alert("You clicked me any help needed");
+});
+
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Today is the react first day";
+}
+
+myFunction();
